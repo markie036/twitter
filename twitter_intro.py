f5a355b5080e32c3c87fd7a84337f85f62878b34
Mark Harrison <mharrisondesign@mail.com>
2018-05-15 11:14:57 +0000

    Addition of another city, code to cross ref any identical trending tags

--- twitter_intro.py
+++ twitter_intro.py
@@ -12,7 +12,16 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 LON_WOE_ID = 44418
+DUB_WOE_ID = 560743
 
 lon_trends = api.trends_place(LON_WOE_ID)
+dub_trends = api.trends_place(DUB_WOE_ID)
 
-print(json.dumps(lon_trends, indent=1))
+# Cross references any identical trends from both cities
+dub_trends_set = set([trend['name'] for trend in dub_trends[0]['trends']])
+
+lon_trends_set = set([trend['name'] for trend in lon_trends[0]['trends']])
+
+common_trends = set.intersection(dub_trends_set, lon_trends_set)
+
+print(common_trends)

564c1f852fbe8424fc05c0dce3499b8eb678412f
Mark Harrison <mharrisondesign@mail.com>
2018-05-15 15:34:53 +0000

    Harvest tweet attributes using double list comprehesions

--- tweet_access.py
+++ tweet_access.py
@@ -11,21 +11,30 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-count = 10
+count = 50
 query = 'Dublin'
 
-#Get all status
+#Get all tweets for the search query
 results = [status for status in tweepy.Cursor(api.search, q=query).items(count)] # a list comprehension
 
-for result in results:
-    print(json.dumps(result._json, indent=4))
-    
-for status in results:
-    print(status.text.encode('utf-8'))
-    print(status.user.id)
-    print(status.user.screen_name)
-    print(status.user.profile_image_url_https)
-    print(status.user.followers_count)
-    print(status.place)
-    
+status_texts = [status._json['text'] for status in results]
+
+screen_names = [ status._json['user']['screen_name']
+                    for status in results
+                        for mention in status._json['entities']['user_mentions'] ]
+                        
+hashtags = [hashtag['text']
+                    for status in results
+                        for hashtag in status._json['entities']['hashtags'] ]
+                        
+words = [ word
+                    for text in status_texts
+                        for word in text.split() ]
+                        
+print(json.dumps(status_texts[0:5], indent=1))
+print(json.dumps(screen_names[0:5], indent=1))
+print(json.dumps(hashtags[0:5], indent=1))
+print(json.dumps(words[0:5], indent=1))
+
+
     
